a0af836d60e24c5dc25774b61b46ff53c9a4bcb9
coderwithattitude <coderwithattitude@gmail.com>
2019-01-08 23:23:17 +0100

    addOrganisation function

--- src/util/db.js
+++ src/util/db.js
@@ -1,6 +1,18 @@
-import {ipfs} from 'ipfs';
+import {IPFS} from 'ipfs';
 import {OrbitDB} from 'orbit-db';
 
-const ipfs = new ipfs();
+const ipfs = new IPFS();
 const orbitdb = new OrbitDB(ipfs);
-const docstore = orbitdb.docstore('payroll-db');
+const docstore = await orbitdb.docstore('payroll-db');
+
+export async function addOrganization(name, admin, employees=1, email,) {
+    try {
+        await docstore.put({ _id: admin, orgname: name, noofemps:employees, email: email });
+    } catch(e) {
+        console.log('error adding org name',e)
+    } 
+
+}
+
+
+
