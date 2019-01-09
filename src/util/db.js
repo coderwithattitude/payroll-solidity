edf7147f366d6962bd78345356285a9cbed425b1
coderwithattitude <coderwithattitude@gmail.com>
2019-01-09 02:34:19 +0100

    added addEmplyee function

--- src/util/db.js
+++ src/util/db.js
@@ -3,17 +3,50 @@ import {OrbitDB} from 'orbit-db';
 
 const ipfs = new IPFS();
 const orbitdb = new OrbitDB(ipfs);
-const docstore = await orbitdb.docstore('payroll-db');
+const docstore = orbitdb.docs('payroll-db');
 
-export async function addOrganization(name, admin, employees=1, email,) {
+export async function addOrganization(name, admin, employeeCount=1, email, employees={}) {
     try {
-        await docstore.put({ _id: admin, orgname: name, noofemps:employees, email: email });
+        await docstore.put({ _id: admin, orgname: name, noofemps:employeeCount, email: email, employees: employees });
         return true;
     } catch(e) {
         console.log('error adding org name',e)
         return false;
     } 
 
+}
+
+export async function getOrganisation(addr) {
+    let org;
+    try {
+        org = await docstore.get(addr);
+    } catch(e) {
+        console.log('Error fetching org:',e);
+    }
+    
+    return org;
+} 
+
+export async function addEmployee(admin,name,addr,rate,minHours,position,organisation) {
+
+    let empObj = {
+        [addr]: {
+            address: addr,
+            name: name,
+            rate: rate,
+            minHours: minHours,
+            position: position,
+            organisation: organisation
+        }
+    }
+try {
+    let payrolldb = docstore.get(admin);
+    await docstore.put({_id: addr, employees: Object.assign(payrolldb.employees, empObj)});
+    return true;
+} catch(e) {
+    console.log('Error adding employee',e);
+    return false;
+}
 }
 
 
