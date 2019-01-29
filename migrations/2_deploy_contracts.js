9e9fe8aa6531ecfeb4f306293b25be483dd68656
coderwithattitude <coderwithattitude@gmail.com>
2019-01-29 13:03:50 +0100

    updated migrations to include daipay.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var SimpleStorage = artifacts.require("SimpleStorage");
 var TutorialToken = artifacts.require("TutorialToken");
 var ComplexStorage = artifacts.require("ComplexStorage");
+var DaiPay = artifacts.require("daipay");
 
 module.exports = function(deployer) {
+  deployer.deploy(DaiPay,"0x1170533e2b679a5dc5038a0F6e525B4aFF04Ee04");
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
