62b055f31249cacd2c3067c8ce54eb58a1717202
coderwithattitude <coderwithattitude@gmail.com>
2019-02-18 15:00:58 +0100

    added store configuration

---
+++ src/store.js
@@ -0,0 +1,42 @@
+import createHistory from 'history/createBrowserHistory';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { routerMiddleware } from 'react-router-redux'
+import reducer from './reducer';
+import rootSaga from './rootSaga';
+import createSagaMiddleware from 'redux-saga';
+import { generateContractsInitialState } from 'drizzle';
+import drizzleOptions from './drizzleOptions';
+
+// instantiate redux devtools to visualize state
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const history = createHistory();
+
+// instantiate middleware objects
+const routingMiddleware = routerMiddleware(history);
+const sagaMiddleware = createSagaMiddleware();
+
+const initialState = {
+  contracts: generateContractsInitialState(drizzleOptions)
+};
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeEnhancers(
+      applyMiddleware(
+        thunkMiddleware,
+        routingMiddleware,
+        sagaMiddleware
+    )
+  )
+);
+
+sagaMiddleware.run(rootSaga)
+
+export { history };
+export { store };
+
+export default store;
+
