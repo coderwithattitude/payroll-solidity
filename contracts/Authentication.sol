bd4b9bb1fa019281d8a5b5026e46cafdfb7d5980
coderwithattitude <coderwithattitude@gmail.com>
2019-02-20 15:46:42 +0100

    hashed inputs

--- contracts/Authentication.sol
+++ contracts/Authentication.sol
@@ -3,11 +3,8 @@ pragma solidity ^0.5.0;
 import "./zeppelin/lifecycle/Killable.sol";
 
 contract Authentication is Killable {
-    struct User {
-        bytes32 name;
-    }
-
-    mapping (address => User) private users;
+  
+    mapping (address => bytes32) private users;
 
     uint private id; // user id temp storage
 
@@ -16,25 +13,29 @@ contract Authentication is Killable {
         return (users[msg.sender].name);
     }
 
-    function signup(bytes32 name) public onlyValidName(name) returns (bytes32) {
-        if (users[msg.sender].name == 0x0) {
-            users[msg.sender].name = name;
-            return (users[msg.sender].name);
+    function signup(bytes32 name, bytes32 salt) public onlyValidName(name) returns (bytes32) {
+        bytes32 org = keccak256(abi.encodePacked(name,salt));
+        if (users[msg.sender] == 0x0) {
+            users[msg.sender] = org;
+
+            return (users[msg.sender]);
         }
 
-        return (users[msg.sender].name);
+        return (users[msg.sender]);
     }
 
-    function update(bytes32 name) public payable onlyValidName(name) onlyExistingUser returns (bytes32) {
-        if (users[msg.sender].name != 0x0) {
-            users[msg.sender].name = name;
+    function update(bytes32 name, bytes32 salt) public payable onlyValidName(name) onlyExistingUser returns (bytes32) {
+        bytes32 org = keccak256(abi.encodePacked(name,salt));
 
-            return (users[msg.sender].name);
+        if (users[msg.sender] != 0x0) {
+            users[msg.sender] = org;
+
+            return (users[msg.sender]);
         }
     }
 
     modifier onlyExistingUser {
-        require(!(user[msg.sender].name == 0x0), "User does npt exist");
+        require(!(user[msg.sender] == 0x0), "User does not exist");
         _;
     }
 
