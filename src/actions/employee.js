ee3430b5f8d397571d1e0789c460e96fc6f5d6e4
coderwithattitude <coderwithattitude@gmail.com>
2019-02-22 19:33:32 +0100

    added delete employee redux action

--- src/actions/employee.js
+++ src/actions/employee.js
@@ -1,27 +1,46 @@
-import { addEmployess, getEmployees, deleteEmployees } from '../utils';
+import { addEmployess, getEmployees, deleteEmployee } from '../utils';
 import { showLoading, hideLoading } from 'react-redux-loading';
 
 export const ADD_EMPLOYEE = 'ADD_EMPLOYEE';
 export const GET_EMPLOYEES = 'GET_EMPLOYEES';
-export const DELETE_EMPLOYEES = 'DELETE_EMPLOYEES';
+export const DELETE_EMPLOYEE = 'DELETE_EMPLOYEE';
 
 
-function addEmployee (employee) {
+function addEmployee(employee) {
     return {
         type: ADD_EMPLOYEE,
         employee,
     }
 }
 
-export function handleAddEmployee (employee) {
+export function handleAddEmployee(employee) {
     return (dispatch, getState) => {
         const { authedUser } = getState();
         
         dispatch(showLoading());
-        return addEmployess({
+        return addEmployee({
             ...employee,
             author: authedUser
         }).then((employee) => dispatch(addEmployee(employee)))
+            .then(() => dispatch(hideLoading()));
+    }
+}
+
+function delEmployee(employee) {
+    return {
+        type: DELETE_EMPLOYEE,
+        employee,
+    }
+}
+
+export function handleDelEmployee(employee) {
+    return (dispatch, getState) => {
+        const { authedUser } = getState();
+        dispatch(showLoading());
+        return deleteEmployee({
+            ...employee,
+            author: authedUser
+        }).then((employee) => dispatch(delEmployee(employee)))
             .then(() => dispatch(hideLoading()));
     }
 }
