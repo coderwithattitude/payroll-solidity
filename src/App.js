41aa6777dc9177c112d0ca50fb8e2e790452ace1
Adibas03 <adibas03@ymail.com>
2019-03-03 21:23:43 +0100

    routes handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // @flow
 
 import * as React from 'react';
-import { Router, browserHistory } from 'react-router';
+import { Router, browserHistory, Redirect, Switch } from 'react-router';
+import { HashRouter, BrowserRouter /*, Router*/, Route } from 'react-router-dom';
+import createBrowserHistory from "history/createBrowserHistory";
+
 import { IntlProvider } from 'react-intl';
 import { IntlProvider as RSIntlProvider } from 'rsuite';
 
@@ -9,16 +12,64 @@ import enGB from 'rsuite/lib/IntlProvider/locales/en_GB';
 import locales from './locales';
 import routes from './routes';
 
+import Frame from './components/Frame';
+
 type Props = {};
+
+const history = createBrowserHistory();
+// const history = browserHistory;
+
+const extractRoute = (route, parent) => {
+  let routes = [];
+
+  const one = {};
+  let path = route.path;
+  path =path && path[0] !== '/' ? '/'+path : path;
+  
+  if (path && path !== '/') {
+    one.path = parent ? parent+path :path
+  }
+  route.component? one.component = route.component : '';
+
+  if (one.path && one.component) {
+    routes.push(one);
+  }
+  let children = [];
+  if (route.childRoutes) {
+    children = Object.keys(route.childRoutes).map ( childKey => extractRoute(route.childRoutes[childKey], path));
+  }
+  routes = routes.concat(children).flat();
+  return routes;
+}
+
+const AdvancedRoutes = props => {
+  return props.routes.map( route => <Route exact={route.exact} key={route.path} path={route.path} component={route.component} />) || <div></div>;
+}
+
+const extractedRoute = extractRoute(routes);
 
 class App extends React.Component<Props> {
   render() {
     return (
-      <IntlProvider locale="en" messages={locales.en}>
-        <RSIntlProvider locale={enGB}>
-          <Router history={browserHistory} routes={routes} />
-        </RSIntlProvider>
-      </IntlProvider>
+        <IntlProvider locale="en" messages={locales.en}>
+          <RSIntlProvider locale={enGB}>
+              <HashRouter>
+                <Frame>
+                  <div>
+                    <Switch>
+                      <Redirect exact from='/' to='/list/members' />
+                    </Switch>
+                  </div>
+                  {
+                    // <Route routes={routes} history={history} />
+                  }
+                  {
+                    <AdvancedRoutes routes={extractedRoute} />
+                  }
+                </Frame>
+              </HashRouter>
+          </RSIntlProvider>
+        </IntlProvider>
     );
   }
 }
