0c6b48beb0ef72764cbfeb039f7711d4ce195a78
coderwithattitude <coderwithattitude@gmail.com>
2019-03-18 14:38:07 +0100

    updated checkbox

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 //import { handleAddOrg } from '../../actions/org';
-import { FlexboxGrid } from 'rsuite';
+import {
+    FlexboxGrid, 
+    Container, 
+    Content, 
+    Form, 
+    FormGroup,
+    ControlLabel,
+    Panel,
+    Input
+ } from 'rsuite';
+
+const { Item } = FlexboxGrid;
 class SignUp extends React.Component<Props, State> {
 
     constructor(props: Props) {
@@ -30,15 +41,46 @@ class SignUp extends React.Component<Props, State> {
 
 
 render () {
+    const { orgName, wallet, email } = this.state;
     return(
         <div className='show-grid'>
-            <FlexboxGrid>
-                <FlexboxGrid.Item className='split-item split-left' colspan={12}>
+            <FlexboxGrid  justify='center'>
+                <Item className='split-item split-left' colspan={12}>
                     <h1><strong>Alkpay</strong></h1>
                     <br/>
                     <h2>Welcome aboard.</h2>
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item justify='center' className='split-item split-right' colspan={12}><div>form</div></FlexboxGrid.Item>
+                </Item>
+                <Item className='split-item split-right' colspan={12}>
+                   <FlexboxGrid  justify='center' align='middle'>
+                   <Item colspan={12}>
+                         <Container>
+                        <Content>
+                            <Panel header={<h1> Create a free Account </h1>}>
+                                 <Form fluid>
+                                    <FormGroup>
+                                         <ControlLabel>Organisation</ControlLabel>
+                                         <Input className= 'form-input'  onChange={this.handleInputChange}  value={ orgName } placeholder="Enter organisation name" name="Organisation" />
+                                    </FormGroup>
+
+                                    <FormGroup>
+                                        <ControlLabel>Email</ControlLabel>
+                                        <Input className= 'form-input' onChange={this.handleInputChange} value={ email } placeholder="Enter email address" name="email"/>
+                                    </FormGroup>  
+                                    <FormGroup>
+                                        <ControlLabel>Wallet Address</ControlLabel>
+                                        <Input className= 'form-input' disabled={true} onChange={this.handleInputChange} value={ wallet } placeholder="enter your ethereum wallet address" name="wallet" />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Input className= 'form-tc' type='checkbox'/>
+                                    </FormGroup>
+                                </Form>
+                            </Panel>
+                        </Content>
+                    </Container>
+                   </Item>
+                   </FlexboxGrid>
+                   
+                </Item>
             </FlexboxGrid>
         </div>
     );
