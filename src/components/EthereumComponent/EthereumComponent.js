081172ac0ea43339d453b89fb4eb623ef0a03cb4
Adibas03 <adibas03@ymail.com>
2019-03-20 22:33:20 +0100

    fill in component

--- src/components/EthereumComponent/EthereumComponent.js
+++ src/components/EthereumComponent/EthereumComponent.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import { FlexboxGrid } from 'rsuite';
+import { Button, Col, FlexboxGrid, Grid, Row } from 'rsuite';
+import Metamasksvg from '../../images/metamask-fox.svg';
 
 
 const EthereumComponent = (WrappedComponent) => props => {
-    console.log(windows)
     console.log(props)
     if (props.web3) {
         return (<WrappedComponent {...props} />);
     } else {
         return(
-            <FlexboxGrid className="backdrop">
-                <FlexboxGrid.Item>
-                    Watcher
+            <FlexboxGrid className="backdrop tight-border" align="middle" justify="center">
+                <FlexboxGrid.Item colspan={8} className="ethereum-box">
+                    <Grid fluid >
+                        <Row>
+                            <Col xs={6} xsOffset={9} className="logo-box">
+                                <img src={Metamasksvg} />
+                            </Col>
+                            <Col md={16} mdOffset={4}>
+                            </Col>
+                        </Row>
+                    </Grid>
+
+                    <div className="details">
+                        <h3>Install Metamask or Ethereum provider to continue</h3>
+                        <p>
+                            You need to install Metamask or visit from an Ethereum enabled browser to pay your workers with Daipay
+                        </p>
+                    </div>
+                    <Grid fluid className="buttonRow">
+                        <Row>
+                            <Col xs={16} xsOffset={4}>
+                                <a href="https://metamask.io" target="_blank">
+                                    <Button block appearance="primary" color="green" className="tight-border">
+                                        INSTALL METAMASK
+                                    </Button>
+                                </a>
+                            </Col>
+                        </Row>
+                    </Grid>
                 </FlexboxGrid.Item>
             </FlexboxGrid>
         )
