58a72cd285823522eac57c200f3ae5740a948c82
adibas03 <adibas03@ymail.com>
2019-04-01 00:04:56 +0100

    move out redux and drizzle leftovers

--- src/components/DrizzleComponent/DrizzleComponent.js
+++ src/components/DrizzleComponent/DrizzleComponent.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { DrizzleProvider } from 'drizzle-react';
+import { LoadingContainer } from 'drizzle-react-components';
 import { Provider } from 'react-redux';
+import LoadingBar from 'react-redux-loading-bar';
+
 import { store } from '../../store';
 import drizzleOptions from '../../drizzleOptions';
 
 const DrizzleComponent = (WrappedComponent) => props => {
 
   return (
-    <DrizzleProvider  options={drizzleOptions}>
-      <Provider store={store}>
-          <WrappedComponent store={store} {...props} />
-      </Provider>
-    </DrizzleProvider>
+        <DrizzleProvider  options={drizzleOptions}>
+          <Provider store={store}>
+            <LoadingContainer>
+              <LoadingBar scope='first'/>
+              <WrappedComponent store={store} {...props} />
+            </LoadingContainer>
+          </Provider>
+        </DrizzleProvider>
   );
 }
 
