8ffee82934b80d61a9ef766b8584cd6a147f1c6b
coderwithattitude <coderwithattitude@gmail.com>
2019-04-01 15:59:53 +0100

    added reducers for org

--- src/reducer.js
+++ src/reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { drizzleReducers } from 'drizzle';
 import { loadingBarReducer } from 'react-redux-loading-bar';
+import org from './reducers/org';
 const reducer = combineReducers({
   routing: routerReducer,
   ...drizzleReducers,
+  org,
   loadingBar: loadingBarReducer
 });
 
