62f32a51232d72b859e4320b972870308acde149
coderwithattitude <coderwithattitude@gmail.com>
2019-04-02 15:48:52 +0100

    added emp reducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { drizzleReducers } from 'drizzle';
 import { loadingBarReducer } from 'react-redux-loading-bar';
+import emps from './reducers/emps';
 const reducer = combineReducers({
+  emps,
   routing: routerReducer,
   ...drizzleReducers,
   loadingBar: loadingBarReducer
